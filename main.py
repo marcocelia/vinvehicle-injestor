c7f7525f9e286535d984373b5db2c8124d358028
Marco Celia <marcocelia@live.com>
2021-01-06 12:39:41 +0100

    complete microservice

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
 import os
 import pandas as pd
-import json
-from kafka import KafkaProducer, KafkaConsumer
+from kafka import KafkaProducer
 
 
 def vin_vehicle_serializer(row):
-    return json.dumps(row.to_json()).encode('utf-8')
+    return ','.join([str(x) for x in row.to_list()]).encode('utf-8')
 
 
 if __name__ == '__main__':
@@ -16,7 +15,8 @@ if __name__ == '__main__':
     producer = KafkaProducer(bootstrap_servers='localhost:9092', value_serializer=vin_vehicle_serializer)
 
     futures = []
-    for i in range(0, 10):
+    for i in range(0, len(data)):
+        print(f"Producing row {i}")
         futures.append(producer.send(topic, data.iloc[i]))
 
     for future in futures:
@@ -24,11 +24,3 @@ if __name__ == '__main__':
 
     print(f"Successfully sent {len(futures)} row")
 
-    # consumer = KafkaConsumer(
-    #     topic,
-    #     bootstrap_servers='localhost:9092',
-    #     value_deserializer=lambda b: json.loads(b),
-    #     auto_offset_reset='earliest'
-    # )
-    # for msg in consumer:
-    #     print(msg.value)
